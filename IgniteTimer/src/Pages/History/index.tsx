79f2948d7ac695e88bc44accc35659a98aaa760e
Jordan Guimarães <jordanggomes@gmail.com>
2024-05-07 14:20:20 -0300

    Page History

--- IgniteTimer/src/Pages/History/index.tsx
+++ IgniteTimer/src/Pages/History/index.tsx
@@ -1,10 +1,55 @@
-import { HistoryContainer } from "./styles"
+import { HistoryContainer, HistoryList } from "./styles"
 
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>History</h1>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+          </tbody>
+
+        </table>
+
+      </HistoryList>
     </HistoryContainer>
   )
 }
